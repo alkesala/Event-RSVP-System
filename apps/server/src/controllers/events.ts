0a4f617e881579fffd91cb7c92125870255afe93
Aleksi Kesälä <nordicdevbox@proton.me>
2025-09-27 15:17:57 +0300

    feat: getbyId events - i fucking love drizzle

--- apps/server/src/controllers/events.ts
+++ apps/server/src/controllers/events.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 import { publicProcedure, protectedProcedure, router } from "@/lib/trpc";
 import { db } from "@/models/database";
+import { eq } from "drizzle-orm";
+import { events } from "@/models/events";
 
 export const EventRouter = router({
   getAll: publicProcedure.query(async () => {
@@ -16,4 +18,12 @@ export const EventRouter = router({
       },
     });
   }),
+
+  getById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      return await db.query.events.findFirst({
+        where: eq(events.id, input.id),
+      });
+    }),
 });
