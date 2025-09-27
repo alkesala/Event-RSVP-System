9b04aebe53a79ffa6bbb3547ce7a1cb409bd69c6
Aleksi Kesälä <nordicdevbox@proton.me>
2025-09-27 15:23:32 +0300

    feat: add event router

--- apps/server/src/controllers/index.ts
+++ apps/server/src/controllers/index.ts
@@ -1,14 +1,15 @@
 import { protectedProcedure, publicProcedure, router } from "../lib/trpc";
-
+import { EventRouter } from "./events";
 export const appRouter = router({
-	healthCheck: publicProcedure.query(() => {
-		return "OK";
-	}),
-	privateData: protectedProcedure.query(({ ctx }) => {
-		return {
-			message: "This is private",
-			user: ctx.session.user,
-		};
-	}),
+  healthCheck: publicProcedure.query(() => {
+    return "OK";
+  }),
+  privateData: protectedProcedure.query(({ ctx }) => {
+    return {
+      message: "This is private",
+      user: ctx.session.user,
+    };
+  }),
+  events: EventRouter,
 });
 export type AppRouter = typeof appRouter;
