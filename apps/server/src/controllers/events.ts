330282986936a70c9603dcc71d1332823e0c83b2
Aleksi Kesälä <nordicdevbox@proton.me>
2025-09-30 14:33:05 +0300

    feat: events routes

--- apps/server/src/controllers/events.ts
+++ apps/server/src/controllers/events.ts
@@ -3,120 +3,120 @@ import { eq } from "drizzle-orm";
 import { createInsertSchema } from "drizzle-zod";
 import { nanoid } from "nanoid";
 import { z } from "zod";
-import { protectedProcedure, publicProcedure, router } from "@/lib/trpc";
+import { protectedProcedure, publicProcedure } from "@/lib/trpc";
 import { db } from "@/models/database";
 import { events } from "@/models/events";
-
+import { router } from "@/lib/trpc";
 const insertEventSchema = createInsertSchema(events).omit({
-	id: true,
-	createdBy: true,
-	createdAt: true,
-	updatedAt: true,
+  id: true,
+  createdBy: true,
+  createdAt: true,
+  updatedAt: true,
 });
 
 // Update is for future
 const _updateEventSchema = createInsertSchema(events)
-	.partial()
-	.omit({ id: true, createdBy: true, createdAt: true, updatedAt: true });
+  .partial()
+  .omit({ id: true, createdBy: true, createdAt: true, updatedAt: true });
 
 export const EventRouter = router({
-	getAll: publicProcedure.query(async () => {
-		try {
-			return await db.query.events.findMany({
-				with: {
-					createdBy: {
-						columns: {
-							id: true,
-							name: true,
-							email: true,
-						},
-					},
-				},
-			});
-		} catch (error) {
-			throw new TRPCError({
-				code: "INTERNAL_SERVER_ERROR",
-				message: "Failed to fetch events",
-				cause: error,
-			});
-		}
-	}),
-	getById: publicProcedure
-		.input(z.object({ id: z.string() }))
-		.query(async ({ input }) => {
-			try {
-				const query = await db.query.events.findFirst({
-					where: eq(events.id, input.id),
-				});
-				if (!query) {
-					throw new TRPCError({
-						code: "NOT_FOUND",
-						message: "Event not found",
-					});
-				}
-				return query;
-			} catch (error) {
-				throw new TRPCError({
-					code: "INTERNAL_SERVER_ERROR",
-					message: "Failed to fetch event",
-					cause: error,
-				});
-			}
-		}),
+  get: publicProcedure.query(async () => {
+    try {
+      return await db.query.events.findMany({
+        with: {
+          createdBy: {
+            columns: {
+              id: true,
+              name: true,
+              email: true,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Failed to fetch events",
+        cause: error,
+      });
+    }
+  }),
+  getById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      try {
+        const query = await db.query.events.findFirst({
+          where: eq(events.id, input.id),
+        });
+        if (!query) {
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Event not found",
+          });
+        }
+        return query;
+      } catch (error) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to fetch event",
+          cause: error,
+        });
+      }
+    }),
 
-	getEventsCreatedByUser: protectedProcedure.query(async ({ ctx }) => {
-		try {
-			const query = await db.query.events.findMany({
-				where: eq(events.createdBy, ctx.session.user.id),
-				with: {
-					rspvs: {
-						with: {
-							user: {
-								columns: {
-									name: true,
-								},
-							},
-						},
-					},
-				},
-			});
-			if (!query) {
-				throw new TRPCError({
-					code: "NOT_FOUND",
-					message: "Events not found",
-				});
-			}
-			return query;
-		} catch (error) {
-			throw new TRPCError({
-				code: "INTERNAL_SERVER_ERROR",
-				message: "Failed to fetch events",
-				cause: error,
-			});
-		}
-	}),
+  getEventsCreatedByUser: protectedProcedure.query(async ({ ctx }) => {
+    try {
+      const query = await db.query.events.findMany({
+        where: eq(events.createdBy, ctx.session.user.id),
+        with: {
+          rspvs: {
+            with: {
+              user: {
+                columns: {
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+      });
+      if (!query) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Events not found",
+        });
+      }
+      return query;
+    } catch (error) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Failed to fetch events",
+        cause: error,
+      });
+    }
+  }),
 
-	create: protectedProcedure
-		.input(insertEventSchema)
-		.mutation(async ({ ctx, input }) => {
-			try {
-				const [newEvent] = await db
-					.insert(events)
-					.values({ id: nanoid(), ...input, createdBy: ctx.session.user.id })
-					.returning();
-				if (!newEvent) {
-					throw new TRPCError({
-						code: "INTERNAL_SERVER_ERROR",
-						message: "Failed to create event",
-					});
-				}
-				return newEvent;
-			} catch (error) {
-				throw new TRPCError({
-					code: "INTERNAL_SERVER_ERROR",
-					message: "Something happened",
-					cause: error,
-				});
-			}
-		}),
+  create: protectedProcedure
+    .input(insertEventSchema)
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const [newEvent] = await db
+          .insert(events)
+          .values({ id: nanoid(), ...input, createdBy: ctx.session.user.id })
+          .returning();
+        if (!newEvent) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Failed to create event",
+          });
+        }
+        return newEvent;
+      } catch (error) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something happened",
+          cause: error,
+        });
+      }
+    }),
 });
--- apps/server/src/controllers/index.ts
+++ apps/server/src/controllers/index.ts
@@ -1,15 +1,16 @@
 import { protectedProcedure, publicProcedure, router } from "../lib/trpc";
 import { EventRouter } from "./events";
+
 export const appRouter = router({
-	healthCheck: publicProcedure.query(() => {
-		return "OK";
-	}),
-	privateData: protectedProcedure.query(({ ctx }) => {
-		return {
-			message: "This is private",
-			user: ctx.session.user,
-		};
-	}),
-	events: EventRouter,
+  healthCheck: publicProcedure.query(() => {
+    return "OK";
+  }),
+  privateData: protectedProcedure.query(({ ctx }) => {
+    return {
+      message: "This is private",
+      user: ctx.session.user,
+    };
+  }),
+  events: EventRouter,
 });
 export type AppRouter = typeof appRouter;
